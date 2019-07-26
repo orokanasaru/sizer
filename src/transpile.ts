8547a3b9d99ff1703a25454f6a4b7a02d1b05358
Michael Hensler <mhensler@microsoft.com>
2019-07-26 14:13:09 -0700

    adding webpack

--- src/transpile.ts
+++ src/transpile.ts
@@ -1,21 +1,25 @@
-import {transpileModule, CompilerOptions} from "typescript";
+import { CompilerOptions, transpileModule } from 'typescript'
 
-export type Options = Pick<CompilerOptions,
-"allowSyntheticDefaultImports"|
-  "downlevelIteration"|
-  "emitDecoratorMetadata"|
-  "esModuleInterop"|
-  "experimentalDecorators"|
-  "importHelpers"|
-  "jsx"|
-  "jsxFactory"|
-  "module"|
-  "preserveConstEnums"|
-  "removeComments"|
-  "target">;
+export type Options = Pick<
+  CompilerOptions,
+  | 'allowSyntheticDefaultImports'
+  | 'downlevelIteration'
+  | 'emitDecoratorMetadata'
+  | 'esModuleInterop'
+  | 'experimentalDecorators'
+  | 'importHelpers'
+  | 'jsx'
+  | 'jsxFactory'
+  | 'module'
+  | 'preserveConstEnums'
+  | 'removeComments'
+  | 'target'
+>
 
 export const transpile = (input: string, options: Options) => {
-  const { diagnostics, outputText } = transpileModule(input, { compilerOptions: options });
+  const { diagnostics, outputText } = transpileModule(input, {
+    compilerOptions: options
+  })
 
-  return { diagnostics, outputText};
-};
+  return { diagnostics, outputText }
+}
---
+++ webpack.config.ts
@@ -0,0 +1,109 @@
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import path from 'path'
+import TerserPlugin from 'terser-webpack-plugin'
+import { BannerPlugin, Configuration, DefinePlugin } from 'webpack'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import webpackMerge from 'webpack-merge'
+
+type EnvironmentOptions = {
+  analyze?: boolean
+  disableConcatenation?: boolean
+}
+
+const analysis: Configuration = {
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      generateStatsFile: true
+    })
+  ]
+}
+
+const disableConcatenation: Configuration = {
+  optimization: {
+    concatenateModules: false
+  }
+}
+
+const getCommonConfig = (mode: Configuration['mode']): Configuration => ({
+  entry: { sizer: './src/sizer.ts' },
+  externals: { vscode: 'commonjs vscode' },
+  mode,
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.build.json',
+          reportFiles: ['src/**/*.{ts,tsx}']
+        },
+        test: /\.tsx?$/
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: ['source-map-loader']
+      }
+    ]
+  },
+  output: {
+    chunkFilename: '[name].js',
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        BUILD_TIMESTAMP: `"${new Date().toISOString()}"`
+      }
+    }),
+    new BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      entryOnly: false,
+      raw: true
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json']
+  },
+  target: 'node'
+})
+
+const getProductionConfig = (): Configuration => ({
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      })
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [new CleanWebpackPlugin()],
+  stats: {
+    maxModules: Infinity
+  }
+})
+
+const getDevelopmentConfig = (): Configuration => ({
+  devtool: 'eval-source-map',
+  output: {
+    path: path.join(__dirname, 'build')
+  },
+  plugins: [new CleanWebpackPlugin()]
+})
+
+module.exports = (
+  env: EnvironmentOptions,
+  { mode = 'development' }: Configuration
+) =>
+  webpackMerge(
+    getCommonConfig(mode),
+    mode === 'production' ? getProductionConfig() : getDevelopmentConfig(),
+    env && env.analyze ? analysis : {},
+    env && env.disableConcatenation ? disableConcatenation : {}
+  )
